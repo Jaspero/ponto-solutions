627febf3efb9047bbea2aa3b4d8c216ddaa07862
flauc <filip.lauc93@gmail.com>
2022-03-02 12:47:13 +0100

    fix: file upload working

--- client/projects/shared/blocks/blocks/utils.tsx
+++ client/projects/shared/blocks/blocks/utils.tsx
@@ -4,14 +4,14 @@ export const IMAGE_DEFINITION = {
 	component: {
 		type: 'image',
 		configuration: {
+			filePrefix: '/public/assets/',
 			uploadMethods: [{
 				id: 'file-manager',
 				label: 'FILE_MANAGER.TITLE',
 				component: JSX(<jms-e-file-manager-select/>),
 				configuration: {
+					route: '/public/assets',
 					hidePath: false,
-					hideFolders: false,
-					allowUpload: false,
 					filters: [{
 						value: file => file.contentType.startsWith('image/')
 					}]
